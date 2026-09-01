cbb991455f6421589ac0a2315e91413d5788db81
GM_2019 <233073388+GM201925@users.noreply.github.com>
2026-09-01 19:36:04 +0800

    Done with lab3 (Fix n from 1024000 to 128000).

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -27,7 +27,7 @@ public class TimeAList {
         AList<Integer> Ns = new AList<>();
         AList<Double> times = new AList<>();
         AList<Integer> opCounts = Ns;
-        for (n = 1000; n <= 1024000; n *= 2) {
+        for (n = 1000; n <= 128000; n *= 2) {
             Ns.addLast(n);
             AList<Integer> list = new AList<>();
 
