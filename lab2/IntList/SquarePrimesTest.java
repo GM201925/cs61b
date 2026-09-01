53dc12b488f3270eaf4f44ceb340a3e4701803c6
GM_2019 <233073388+GM201925@users.noreply.github.com>
2026-09-01 11:56:36 +0800

    Done with lab2.

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,34 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimes2() {
+        /**
+         * To test it can judge 1 to be composite.
+         */
+        IntList lst = IntList.of(1, 1, 1);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 1 -> 1", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void testAllPrimes() {
+        /**
+         * Only square the first prime ?
+         */
+        IntList lst = IntList.of(7, 11, 29, 13);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("49 -> 121 -> 841 -> 169", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testOnlyOneElement() {
+        IntList lst = IntList.of(7);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("49", lst.toString());
+        assertTrue(changed);
+    }
 }
